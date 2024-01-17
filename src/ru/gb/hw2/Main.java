58638eea89219c121eca3755080756e6c8faaf6c
caregor <64469336+caregor@users.noreply.github.com>
2024-01-17 10:19:17 +0500

    Task 1 added javadoc.

--- src/ru/gb/hw2/Main.java
+++ src/ru/gb/hw2/Main.java
@@ -1,8 +1,11 @@
 package ru.gb.hw2;
 
-import java.lang.reflect.Array;
+
 import java.util.Arrays;
 
+/**
+ * 1. Написать метод, возвращающий количество чётных элементов массива.
+ */
 public class Main {
     public static void main(String[] args) {
         int[] myArray = {3,5,6,7,8,9,2,3,5,6,12,56,84,110};
@@ -10,6 +13,13 @@ public class Main {
         System.out.printf("Even count is %d of %d",result, myArray.length);
     }
 
+    /**
+     * Задача № 1
+     * Метод, использующий Stream API для подсчета чётных элементов в массиве.
+     *
+     * @param input Массив целых чисел.
+     * @return Количество чётных элементов в массиве.
+     */
     public static int countEvens(int[] input){
         return (int) Arrays.stream(input).filter(element -> element % 2 == 0).count();
     }
