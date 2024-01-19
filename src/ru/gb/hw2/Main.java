d8621965a24f005b179c8c09fb028c084cbc94fa
caregor <64469336+caregor@users.noreply.github.com>
2024-01-19 10:00:18 +0500

    Task 3 is done.

--- src/ru/gb/hw2/Main.java
+++ src/ru/gb/hw2/Main.java
@@ -10,13 +10,15 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
-        int[] myArray = {3,5,6,7,8,9,2,3,5,6,12,56,84,110};
+        int[] myArray = {3,5,6,7,8,9,2,3,5,6,12,56,84,110, 0, 0};
 
         int result = countEvens(myArray);
         int diff = difference(myArray);
+        boolean doubleZero = zeroElements(myArray);
 
         System.out.printf("Even count is %d of %d \n",result, myArray.length);
-        System.out.printf("Difference between maximum and minimum %d", diff);
+        System.out.printf("Разница между минимальным и максимальным элементом: %d \n", diff);
+        System.out.printf("Два нулевых значения? %b \n", doubleZero);
     }
 
     /**
@@ -47,5 +49,27 @@ public class Main {
         int min = Arrays.stream(input).min().orElseThrow();
 
         return max - min;
+    }
+
+    /**
+     * Функция для проверки наличия двух соседних элементов в массиве,
+     * с нулевым значением.
+     *
+     * @param input Массив целых чисел.
+     * @return true, если есть два соседних элемента, один из которых равен нулю, иначе false.
+     * @throws IllegalArgumentException если массив содержит менее двух элементов.
+     */
+    public static boolean zeroElements(int[] input) {
+        if (input == null || input.length < 2) {
+            throw new IllegalArgumentException("Массив должен содержать как минимум два элемента.");
+        }
+
+        for (int i = 0; i < input.length - 1; i++) {
+            if (input[i] == 0 && input[i + 1] == 0) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
